5a2765faeea4e501177ac36603b463d2b9e07e35
Mehmet Demir <demorapps@gmail.com>
2022-06-04 08:26:11 +0300

    Contracts of Data Sources

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -4,3 +4,8 @@ abstract class Failure extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
+// General Failues
+class ServerFailures extends Failure {}
+
+class CacheFailures extends Failure {}
---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_tdd_clean_architecture/core/error/failures.dart';
+import 'package:flutter_tdd_clean_architecture/core/platform/network_info.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl(
+      {required this.remoteDataSource,
+      required this.localDataSource,
+      required this.networkInfo});
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    // TODO: implement getConcreteNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRondomNumberTrivia() {
+    // TODO: implement getRondomNumberTrivia
+    throw UnimplementedError();
+  }
+}
